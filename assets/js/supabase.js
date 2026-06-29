91dbea7b1d8c4a5732ddb3c11063d8e78e3c66fe
iBeenBS <ivandacruzsilvasantoss@gmail.com>
2026-06-29 11:58:56 -0300

    Projeto inicial

---
+++ assets/js/supabase.js
@@ -0,0 +1,9 @@
+// Arquivo de configuração do supabase
+
+const supabaseUrl = "https://endocmamlbvrudlkjpiv.supabase.co";
+const supabaseKey = "sb_publishable_JSHdhGJ2jPnfvjpesOqhcg_qIUUbVlI";
+
+const supabase = window.supabase.createClient(
+	supabaseUrl,
+	supabaseKey
+);
